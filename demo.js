1d2bfe6b9895313d4e15f5057ab58a821dc73ace
baukajikorean <bawka9696@gmail.com>
2019-01-23 19:11:51 +0500

    update

---
+++ demo.js
@@ -0,0 +1,25 @@
+var images = [
+    '1.jpg', 
+    '2.jpg', 
+    '3.jpg'
+];
+
+var num = 0;
+
+function prev() {
+	var slider = document.getElementById('slider');
+	num--;
+	if (num < 0) {
+		num = images.length - 1;
+	}
+	slider.src = images[num];
+}
+
+function next() {
+	var slider = document.getElementById('slider');
+	num++;
+	if (num >= images.length) {
+		num = 0;
+	}
+	slider.src = images[num];
+}
